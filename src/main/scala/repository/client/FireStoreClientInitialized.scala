988a16caa50a17ef12f5aa75c0f6dfc4a1f541a1
nakao keisuke <r3grashopper193@gmail.com>
2022-02-24 22:27:53 +0900

    fix hard-coding to final val

--- src/main/scala/repository/client/FireStoreClientInitialized.scala
+++ src/main/scala/repository/client/FireStoreClientInitialized.scala
@@ -5,14 +5,11 @@ import com.google.cloud.firestore.Firestore
 import com.google.firebase.cloud.FirestoreClient
 import com.google.firebase.{FirebaseApp, FirebaseOptions}
 
-import java.io.FileInputStream
-
 // TODO: クライアントのメソッドをラップしたメソッド作成
 case class FireStoreClientInitialized() {
 
   // TODO: コンパニオンオブジェクトの中に記載するか検討
-  private val serviceAccount = new FileInputStream("/Users/ksk/Downloads/slack-to-backlog-933bd-firebase-adminsdk-t3lgn-6a2b4af488.json")
-  private val credentials = GoogleCredentials.fromStream(serviceAccount)
+  private val credentials = GoogleCredentials.fromStream(getClass.getClassLoader.getResourceAsStream(sys.env("FIREBASE_SETTING_JSON")))
   private val options = FirebaseOptions.builder().setCredentials(credentials).build
   FirebaseApp.initializeApp(options)
 
--- src/main/scala/repository/impl/FireStoreRepositoryImpl.scala
+++ src/main/scala/repository/impl/FireStoreRepositoryImpl.scala
@@ -10,12 +10,18 @@ import javax.inject.Inject
 
 // TODO: ハードコーディング修正（FireStoreの構造に依存する = FireStoreRepositoryImplに依存するため、ここにstatic valueとして記載する）
 case class FireStoreRepositoryImpl @Inject() (fireStoreClient: FireStoreClientInitialized) extends StoreRepository {
+  //  users collection keys
+  private final val users = "users"
+  private final val spaceId = "spaceId"
+  private final val apiKey = "apiKey"
+  //  links collection keys
+  private final val links = "links"
 
   private val db = fireStoreClient.fireStore
 
   // TODO: getMostRecentMessageLinkを参考にリファクタ
   override def getBacklogAuthInfo(teamId :String, userId: String): BacklogAuthInfoEntity = {
-    val query = db.collection("users").document(teamId)
+    val query = db.collection(users).document(teamId)
 
     if (query.get().get().get(userId) == null) {
       return BacklogAuthInfoEntity.apply("","")
@@ -25,7 +31,7 @@ case class FireStoreRepositoryImpl @Inject() (fireStoreClient: FireStoreClientIn
     val map = doc.substring(1, doc.length - 1).replace(" ", "").split(",").map(_.split("="))
       .map { case Array(k, v) => (k, v)}.toMap
 
-    BacklogAuthInfoEntity.apply(map("spaceId"),map("apiKey"))
+    BacklogAuthInfoEntity.apply(map(spaceId),map(apiKey))
   }
 
   override def createBacklogAuthInfo(teamId :String, userId: String, req: ViewSubmissionRequest): Unit = {
@@ -34,8 +40,8 @@ case class FireStoreRepositoryImpl @Inject() (fireStoreClient: FireStoreClientIn
     // JavaのhashMapを渡す必要あり
     val tmpAuthInfo = new util.HashMap[String, String] {
       {
-        put("spaceId", spaceId)
-        put("apiKey", apiKey)
+        put(spaceId, spaceId)
+        put(apiKey, apiKey)
       }
     }
     val param = new util.HashMap[String, util.HashMap[String, String]] {
@@ -43,7 +49,7 @@ case class FireStoreRepositoryImpl @Inject() (fireStoreClient: FireStoreClientIn
         put(userId, tmpAuthInfo)
       }
     }
-    db.collection("users").document(teamId).set(param).get()
+    db.collection(users).document(teamId).set(param).get()
   }
 
   override def createMostRecentMessageLink(teamId: String, userId: String, url: String): Unit = {
@@ -53,11 +59,11 @@ case class FireStoreRepositoryImpl @Inject() (fireStoreClient: FireStoreClientIn
         put(userId, url)
       }
     }
-    db.collection("links").document(teamId).set(param).get()
+    db.collection(links).document(teamId).set(param).get()
   }
 
   override def getMostRecentMessageLink(teamId: String, userId: String): String = {
-    val query = db.collection("links").document(teamId)
+    val query = db.collection(links).document(teamId)
     val doc = query.get().get().get(userId)
     doc.toString
   }
